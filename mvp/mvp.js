18e8f27466f3572335d742e5eca8c07c29bf30d9
voidSatisfaction <lourie@naver.com>
2018-02-02 23:13:18 +0900

    feat: add mvp concept

--- mvp/mvp.js
+++ mvp/mvp.js
@@ -1,19 +1,65 @@
-(function() {
-  const $bookmarkList = document.querySelector('.bookmark-list');
+class Bookmark {
+  constructor (opts) {
+    this.url = opts.url;
+    this.created = opts.created;
+  }
 
-  const $bookmarkInput = document.querySelector('.bookmark-input');
-  const $bookmarkButton = document.querySelector('.bookmark-button');
-
-  $bookmarkButton.addEventListener('click', () => {
-    const $bookmark = bookmarkElement($bookmarkInput.value);
-    $bookmarkList.appendChild($bookmark);
-  });
-})();
-
-function bookmarkElement(bookmark) {
-  const $list = document.createElement('li');
-  const $textNode = document.createTextNode(bookmark);
-  $list.appendChild($textNode);
-
-  return $list;
+  getRelativeTime() {
+    return moment(this.created).fromNow();
+  }
 }
+
+class BookmarkView {
+  constructor ($bookmarkList) {
+    this.$bookmarkList = $bookmarkList;
+    this.$bookmarkInput = document.querySelector('.bookmark-input');
+    this.$bookmarkButton = document.querySelector('.bookmark-button');
+
+    this.$bookmarkButton.addEventListener('click', () => {
+      const url = this.$bookmarkInput.value;
+      const addNewBookmarkEvent = new CustomEvent('addNewBookmark', { detail: { url: url } });
+      console.log(addNewBookmarkEvent);
+      document.dispatchEvent(addNewBookmarkEvent);
+    });
+  }
+
+  render (bookmarks) {
+    console.log(bookmarks);
+    this.$bookmarkList.innerHTML = '';
+    bookmarks.forEach((bookmark) => {
+      const $bookmark = this._bookmarkElement(bookmark.url);
+      this.$bookmarkList.appendChild($bookmark);
+    });
+  }
+
+  _bookmarkElement(url) {
+    const $list = document.createElement('li');
+    const $textNode = document.createTextNode(url);
+    $list.appendChild($textNode);
+
+    return $list;
+  }
+}
+
+class BookmarkPresenter {
+  constructor ($element) {
+    this.bookmarks = [];
+    this.view = new BookmarkView($element);
+    document.addEventListener('addNewBookmark', (e) => {
+      this.addNewBookmark(e.detail.url);
+    })
+  }
+
+  addNewBookmark(url) {
+    const bookmark = new Bookmark({
+      url: url,
+      created: new Date(),
+    });
+    this.bookmarks.push(bookmark);
+    this.view.render(this.bookmarks);
+  }
+}
+
+(function() {
+  const presenter = new BookmarkPresenter(document.querySelector('.bookmark-list'));
+})();
